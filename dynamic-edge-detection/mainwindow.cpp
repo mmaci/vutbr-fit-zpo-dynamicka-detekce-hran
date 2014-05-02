824649e359b2b1c57aaf50419d68d92b5bc435a5
Pavel Macenauer <pavel.macenauer@fotoaparat.cz>
2014-05-02 13:43:51 +0200

    an image displays after loading

--- dynamic-edge-detection/mainwindow.cpp
+++ dynamic-edge-detection/mainwindow.cpp
@@ -12,3 +12,13 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString filename = QFileDialog::getOpenFileName(this, tr("Open File ..."));
+
+    QImage image;
+    image.load(filename);
+
+    ui->displayArea->setPixmap(QPixmap::fromImage(image));
+}
--- dynamic-edge-detection/mainwindow.h
+++ dynamic-edge-detection/mainwindow.h
@@ -2,21 +2,25 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-    
+    public:
+        explicit MainWindow(QWidget *parent = 0);
+        ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
 private:
-    Ui::MainWindow *ui;
+        Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
