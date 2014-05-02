180239194cea817bbc2fd6b86ee8f16f47f32500
Johny231 <Johny231@centrum.cz>
2014-05-02 21:36:23 +0200

    tutorial

--- dynamic-edge-detection/mainwindow.cpp
+++ dynamic-edge-detection/mainwindow.cpp
@@ -15,10 +15,52 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QString filename = QFileDialog::getOpenFileName(this, tr("Open File ..."));
+   // QString filename = QFileDialog::getOpenFileName(this, tr("Open File ..."));
 
-    QImage image;
-    image.load(filename);
+   //QPixmap pix = ui->imgVstup->pixmap();
 
-    ui->displayArea->setPixmap(QPixmap::fromImage(image));
+    vystupImage = vstupImage;//kvuli alokaci rozmeru - asi jde jinak
+    for(int x=0; x < vstupImage.width(); x++)
+    {
+        for(int y=0; y < vstupImage.height(); y++)
+        {
+            QColor pixlik(vstupImage.pixel(x,y));
+            //tady nejaka funkce
+            //.........
+            int r,g,b;
+            r = 255 - pixlik.red();
+            g = 255 - pixlik.green();
+            b = 255 - pixlik.blue();
+            //.........
+            QColor vystup(r,g,b);
+            vystupImage.setPixel(x,y,vystup.rgb());
+        }
+
+    }
+    QPixmap pix = QPixmap::fromImage(vystupImage);
+    ui->imgVystup->setPixmap(pix.scaled(ui->imgVystup->width(),ui->imgVystup->height(),Qt::KeepAspectRatio));
+
+}
+
+void MainWindow::on_actionUlo_it_triggered()
+{
+  QString filename = QFileDialog::getSaveFileName(this, tr("Save File ..."),"","*.jpg");
+  vystupImage.save(filename,"JPEG");
+
+}
+
+void MainWindow::on_actionOtev_t_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, tr("Open File ..."),"","*.jpg");
+
+    vstupImage.load(filename);
+//p.scaled(w,h,Qt::KeepAspectRatio
+
+    QPixmap pix = QPixmap::fromImage(vstupImage);
+    ui->imgVstup->setPixmap(pix.scaled(ui->imgVstup->width(),ui->imgVstup->height(),Qt::KeepAspectRatio));
+}
+
+void MainWindow::on_actionKonec_triggered()
+{
+    close();
 }
--- dynamic-edge-detection/mainwindow.h
+++ dynamic-edge-detection/mainwindow.h
@@ -12,15 +12,25 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
-    public:
-        explicit MainWindow(QWidget *parent = 0);
+    public:    
+    explicit MainWindow(QWidget *parent = 0);
         ~MainWindow();
 
+
 private slots:
+
     void on_pushButton_clicked();
+
+    void on_actionUlo_it_triggered();
+
+    void on_actionOtev_t_triggered();
+
+    void on_actionKonec_triggered();
 
 private:
         Ui::MainWindow *ui;
+        QImage vstupImage;
+        QImage vystupImage;
 };
 
 #endif // MAINWINDOW_H
